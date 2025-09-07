2747f0f97243d7a682be12b5b6d7f106239c567a
Alexandrol20220521 <turuwatari@gmail.com>
2025-09-07 16:10:02 +0900

    loading view created

---
+++ World Wide Wave/LoadingView.swift
@@ -0,0 +1,74 @@
+//
+//  LoadingView.swift
+//  World Wide Wave
+//
+//  Created by Ryutarou Nakajima on 2025/09/07.
+//
+
+import SwiftUI
+import UIKit
+
+struct UIKitRootViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = storyboard.instantiateViewController(identifier: "TabBarController")
+        
+        return rootVC
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+struct LoadingView: View {
+    @State private var scale: CGFloat = 0.1
+    @State private var opacity: CGFloat = 1.0
+    var onFInished: () -> Void
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .cyan, .white, .brown]),
+                          startPoint: .bottomTrailing,
+                          endPoint: .topLeading)
+            .ignoresSafeArea()
+            
+            Image("Logo")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .scaleEffect(scale)
+                .opacity(opacity)
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 2.0).repeatForever(autoreverses: true)) {
+                self.scale = 3.0
+                self.opacity = 0.0
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                onFInished()
+            }
+        }
+    }
+}
+
+struct LoadingViewWrapper: View {
+    @State private var finished: Bool = true
+    
+    var body: some View {
+        if finished {
+            UIKitRootViewControllerRepresentable()
+        } else {
+            LoadingView {
+                finished = true
+            }
+        }
+    }
+}
+#Preview {
+    LoadingView( onFInished: {})
+}
