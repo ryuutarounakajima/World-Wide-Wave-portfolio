28014909c87fbd5c308de27b0b4aced2f0a969a6
Ryuutarou Nakajima <turuwatari@gmail.com>
2025-10-27 18:06:25 +0900

    2025/10/27 mylog view edited

--- World Wide Wave/Mylog/MylogSwiftUIView.swift
+++ World Wide Wave/Mylog/MylogSwiftUIView.swift
@@ -9,6 +9,30 @@ import SwiftUI
 import SwiftData
 import AVKit
 
+
+struct AssetScrollView: View {
+    
+    let assetImages = ["wave1", "wave2", "wave3", "wave4", "wave5"]
+    
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: true) {
+            HStack(spacing: 16) {
+                ForEach(assetImages, id: \.self) {
+                    imageName in
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .clipped()
+                        .cornerRadius(180)
+                        .shadow(radius: 5)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .frame(height: 140)
+    }
+}
 struct MylogSwiftUIView: View {
     
     
@@ -17,59 +41,80 @@ struct MylogSwiftUIView: View {
     
     var body: some View {
         NavigationStack {
-            if logs.isEmpty {
-                 
-                VStack(spacing: 12) {
-                    Image("Logo")
-                        .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 360, height: 360)
-                                                             .opacity(0.7)
-                                            Text("No logs yet 🌊")
-                                                .font(.headline)
-                              .foregroundColor(.gray)
-                    Spacer()
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                .background(Color(.systemGroupedBackground))
-                                .navigationTitle("My surf logs")
+            GeometryReader { geometry in
                 
-            } else {
-                List {
-                    ForEach(logs) { log in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                
+                if logs.isEmpty {
+                    
+                    VStack(spacing: height * 0.06) {
+                        Image("Logo")
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(180)
+                            .frame(width: width * 0.7)
+                            .opacity(0.7)
                         
-                        VStack(alignment: .leading, spacing: 8) {
-                            if let  imageData = log.imageData, let UIImage = UIImage(data: imageData) {
+                        Text("No logs yet 🌊")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        
+                        VStack(alignment: .leading, spacing: height * 0.015) {
+                            
+                            Text("Wave Gallery")
+                                .font(.title)
+                                .bold()
+                                .padding(.vertical)
+                                .padding(.leading)
+                                .foregroundStyle(.primary)
+                            
+                            AssetScrollView()
                                 
-                                Image(uiImage: UIImage)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(height: 100)
-                                    .clipped()
-                                    .cornerRadius(12)
-                            } else {
-                                Image("Logo")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(height: 180)
-                                    .clipped()
-                                    .cornerRadius(12)
-                                    .overlay(
-                                        Text("No Image Available")
-                                            .font(.caption)
-                                            .foregroundColor(.white)
-                                            .padding(6)
-                                            .background(Color.black.opacity(0.5))
-                                            .cornerRadius(8),
-                                        alignment: .bottomTrailing
-                                    )
+                        }
+                        
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(.systemGroupedBackground))
+                    .navigationTitle("My surf logs")
+                    
+                } else {
+                    List {
+                        ForEach(logs) { log in
+                            
+                            VStack(alignment: .leading, spacing: 8) {
+                                if let  imageData = log.imageData, let UIImage = UIImage(data: imageData) {
+                                    
+                                    Image(uiImage: UIImage)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(height: 100)
+                                        .clipped()
+                                        .cornerRadius(12)
+                                } else {
+                                    Image("Logo")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(height: 180)
+                                        .clipped()
+                                        .cornerRadius(12)
+                                        .overlay(
+                                            Text("No Image Available")
+                                                .font(.caption)
+                                                .foregroundColor(.white)
+                                                .padding(6)
+                                                .background(Color.black.opacity(0.5))
+                                                .cornerRadius(8),
+                                            alignment: .bottomTrailing
+                                        )
+                                }
                             }
                         }
                     }
+                    
                 }
-                
             }
-                
         }
     }
 }
